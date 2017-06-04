9d6c40f5f3d0c9be6524b56be6edbd22a33258bf
tu4mo <tu4mo@users.noreply.github.com>
2017-06-04 17:43:11 +0300

    Add theme support for post thumbnails

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 function theme_setup() {
   add_theme_support( 'menus' );
   add_theme_support( 'title-tag' );
+  add_theme_support( 'post-thumbnails' );
 }
 
 add_action( 'after_setup_theme', 'theme_setup' );
